bd94edf636a3a872289d865226cb27a9bd5b0af5
assem2023-habib <099assemhb@gmail.com>
2025-11-26 21:11:16 +0300

    make full screen for application

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from ui.app import RectPackApp
+from PySide6.QtGui import QFont
+from ui.components.combined_components_demo import CombinedDemoWindow
 
 def main():
     app = QApplication(sys.argv)
-    window = RectPackApp()
-    window.show()
+    app.setFont(QFont("Segoe UI", 10))
+    
+    window = CombinedDemoWindow()
+    window.showMaximized()
     sys.exit(app.exec())
 
 if __name__ == "__main__":
